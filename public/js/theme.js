13de1a6fc8d58fc8cb1aa913cf325b6724f58a20
iscii <iscii@users.noreply.github.com>
2021-01-18 14:44:28 -0500

    theme

---
+++ public/js/theme.js
@@ -0,0 +1,13 @@
+function inittheme() {
+    let theme = localStorage.getItem("theme");
+    if (theme) setTheme(theme);
+    document.getElementById("themeswitch").getElementsByTagName("input")[0].checked = theme == "light";
+}
+function setTheme(theme) {
+    localStorage.setItem('theme', theme);
+    document.documentElement.className = theme;
+}
+function toggleTheme() {
+    let theme = localStorage.getItem("theme") == "dark" ? "light" : "dark";
+    setTheme(theme);
+}
