bb9ba28a3e24a4ce631035f2d0dec41ef9424003
iscii <iscii@users.noreply.github.com>
2021-01-22 22:46:04 -0500

    categories and content

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,46 @@
 function init() {
     
 }
+//can totally simplify the loads into one function
+function loadL1() {
+    let content=document.getElementById("content");
+    content.innerHTML="<object class='levelcontent' type='text/html' data='Level 1/index.html'></object>";
+    content.className+="content-level";
+    document.getElementById("content-desc").innerHTML="Level 1";
+}
+function loadL2() {
+    let content=document.getElementById("content");
+    content.innerHTML="<object class='levelcontent' type='text/html' data='Level 2/index.html'></object>";
+    content.className+="content-level";
+    document.getElementById("content-desc").innerHTML="Level 2";
+}
+function loadL3() {
+    let content=document.getElementById("content");
+    content.innerHTML="<object class='levelcontent' type='text/html' data='Level 3/index.html'></object>";
+    content.className+="content-level";
+    document.getElementById("content-desc").innerHTML="Level 3";
+}
+function loadL4() {
+    let content=document.getElementById("content");
+    content.innerHTML="<object class='levelcontent' type='text/html' data='Level 4/index.html'></object>";
+    content.className+="content-level";
+    document.getElementById("content-desc").innerHTML="Level 4";
+}
+function loadL5() {
+    let content=document.getElementById("content");
+    content.innerHTML="<object class='levelcontent' type='text/html' data='Level 5/index.html'></object>";
+    content.className+="content-level";
+    document.getElementById("content-desc").innerHTML="Level 5";
+}
+function loadL6() {
+    let content=document.getElementById("content");
+    content.innerHTML="<object class='levelcontent' type='text/html' data='Level 6/index.html'></object>";
+    content.className+="content-level";
+    document.getElementById("content-desc").innerHTML="Level 6";
+}
+function loadL7() {
+    let content=document.getElementById("content");
+    content.innerHTML="<object class='levelcontent' type='text/html' data='Level 7/index.html'></object>";
+    content.className+="content-level";
+    document.getElementById("content-desc").innerHTML="Level 7";
+}
