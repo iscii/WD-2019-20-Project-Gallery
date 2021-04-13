83c2b83585160fc0c69129e41c98076dfc786ede
iscii <iscii@users.noreply.github.com>
2021-04-13 10:06:43 -0400

    updates

--- public/js/script.js
+++ public/js/script.js
@@ -4,14 +4,34 @@ function init() {
 
 }
 
-function loadLevel(level) {
+function displayContent(cat) {
+    let content = document.getElementById("content");
+    switch (cat){
+        case "links":
+            document.getElementById("content-desc").innerHTML = "Links";
+            if(content.lastChild) content.removeChild(content.lastChild);
+        break;
+        case "webdev":
+            document.getElementById("content-desc").innerHTML = "Web Development";
+            if(content.lastChild) content.removeChild(content.lastChild);
+        break;
+        case 'games':
+            document.getElementById("content-desc").innerHTML = "Games";
+            if(content.lastChild) content.removeChild(content.lastChild);
+        break;
+    }
+}
+
+function displayLevel(level) {
+    document.getElementById("content-desc").innerHTML = "Level " + level + "\n" + LEVELDESC[level - 1];
     let content = document.getElementById("content");
     var obj = document.createElement("object");
     obj.type = "text/html";
     obj.data = "Web Development/Level " + level + "/index.html";
-    obj.className += "levelcontent";
-    content.replaceChild(obj, content.lastChild);
-    document.getElementById("content-desc").innerHTML = "Level " + level + "\n" + LEVELDESC[level - 1];;
+    obj.className += "levelContent";
+    if(content.lastChild)  
+        return content.replaceChild(obj, content.lastChild);
+    content.appendChild(obj);
 }
 
 function dropDown(catEl) {
@@ -19,8 +39,4 @@ function dropDown(catEl) {
     if(items.contains("cat-expanded"))
         return items.remove("cat-expanded");
     items.add("cat-expanded");
-}
-
-function display() {
-
 }
